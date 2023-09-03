9e1c734f5081a9b5dcb7ea0fb5ede0567b871471
Axel Taussig <axeltaussig@gmail.com>
2023-09-03 19:21:17 +1000

    Mobile Adjustments

--- src/containers/sliderConfig.jsx
+++ src/containers/sliderConfig.jsx
@@ -3,16 +3,9 @@ import React from 'react';
 import arrowPrev from '../assets/images/buttons/ArrowsArtboard 1.svg'
 import arrowNext from '../assets/images/buttons/ArrowsArtboard 1 copy.svg'
 
-export const buttonStyle = {
-    width: "30px",
-    background: 'none',
-    border: '0px',
-    margin: '0 50px',
-};
-
 export const properties = {
-    prevArrow: <button style={{ ...buttonStyle }}><img src={arrowPrev} alt=""/></button>,
-    nextArrow: <button style={{ ...buttonStyle }}><img src={arrowNext} alt=""/></button>
+    prevArrow: <button className='button-style'><img src={arrowPrev} alt=""/></button>,
+    nextArrow: <button className='button-style'><img src={arrowNext} alt=""/></button>
 }
 
 export  const slideConfig = {
